eb12eb023a9fa61108ecb97ce0801f903faf92d4
Virssello <56563209+Virssello@users.noreply.github.com>
2022-04-26 19:30:12 +0200

    Readme update

--- components/config/config.ts
+++ components/config/config.ts
@@ -1,11 +1,11 @@
 const config = {
     firebase: {
-        apiKey: "AIzaSyBdr-WOLFV_eHPp3iBc9CkxLXNxLuWHr5w",
-        authDomain: "blikv2pam.firebaseapp.com",
-        projectId: "blikv2pam",
-        storageBucket: "blikv2pam.appspot.com",
-        messagingSenderId: "376857140310",
-        appId: "1:376857140310:web:9fdda70dd053dcc54ceff7"
+        apiKey: "x",
+        authDomain: "x",
+        projectId: "x",
+        storageBucket: "x",
+        messagingSenderId: "x",
+        appId: "x"
     }
 }
 export default config;
